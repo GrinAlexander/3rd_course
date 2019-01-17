3dc1e3d92f230b2c95aaadfc648aa37c9d4494f0
Alexander Grin <33058645+GrinAlexander@users.noreply.github.com>
2019-01-18 00:48:23 +0300

    UPD: 18.01.2019
    Added the 18s lab

---
+++ KP_Labs/18/18_1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class Film
+    {
+        public string Name {get; set;}
+        public int Duration { get; set; }
+        public string Genre { get; set; }
+
+        public Film (string name, int duration, string genre)
+        {
+            Name = name;
+            Duration = duration;
+            Genre = genre;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            First();
+            Second();
+        }
+        static void First()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Задание 1:");
+            try
+            {
+                List<long> list = new List<long> { 6, -15, 9, -9 };
+                List<long> tempList = new List<long>();
+                foreach (long item in list)
+                {
+                    tempList.Add(Math.Abs(item));
+                }
+                list.AddRange(tempList);
+
+                Console.WriteLine("\nЗадание 1.1:");
+                foreach (long item in list)
+                {
+                    Console.Write($"{item} ");
+                }
+
+                short sh = 228;
+                list.Add((long)sh);
+                Console.WriteLine("\nЗадание 1.2:");
+                foreach (long item in list)
+                {
+                    Console.Write($"{item} ");
+                }
+
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        list.RemoveAt(i);
+                    }
+                }
+
+                Console.WriteLine("\nЗадание 1.3:");
+                foreach (long item in list)
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+            catch
+            {
+
+                Console.WriteLine("Ошибка!");
+            }
+            
+            Console.ReadLine();
+        }
+
+        static void Second()
+        {
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Задание 2:");
+            try
+            {
+                List<Film> films = new List<Film>();
+                bool correctness = false;
+
+                Console.WriteLine("Задание 2.1:");
+                Console.Write("Количество фильмов: ");
+                int n = 0, duration = 0;
+                string name = null, genre = null;
+                
+                while (!correctness)
+                { 
+                    try
+                    {
+                        n = Convert.ToInt32(Console.ReadLine());
+                        correctness = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Некорректное значение!");
+                        correctness = false;
+                        
+                    }
+                }
+
+                correctness = false;
+                for (int i = 0; i < n; i++)
+                {
+                    while (!correctness)
+                    {
+                        try
+                        {
+                            Console.Write("Название: ");
+                            name = Console.ReadLine();
+                            Console.Write("Длительность: ");
+                            duration = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Жанр: ");
+                            genre = Console.ReadLine();
+                            correctness = true;
+
+                        }
+                        catch
+                        {
+
+                            Console.WriteLine("Попробуй ещё раз!");
+                            correctness = false;
+                        }
+                    }
+                    films.Add(new Film(name, duration, genre));
+                    correctness = false;
+                }
+
+                int minDuration = Int32.MaxValue, maxDuration = Int32.MinValue;
+                foreach (Film film in films)
+                {
+                    if (film.Genre.ToLower() == "боевик")
+                    {
+                        if (film.Duration < minDuration)
+                        {
+                            minDuration = film.Duration;
+                        }
+                        if (film.Duration > maxDuration)
+                        {
+                            maxDuration = film.Duration;
+                        }
+                    }
+                }
+
+                films.Add(new Film("Война бесконечности", (maxDuration - minDuration) * 2, "Боевик"));
+                Console.WriteLine($"Фильмов в списке: {films.Count}");
+                foreach (Film film in films)
+                {
+                    Console.WriteLine($"\nНазвание: {film.Name} \nДлительность: {film.Duration} \nЖанр: {film.Genre}");
+                }
+
+            }
+            catch
+            {
+
+                Console.WriteLine("Ошибка!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KP_Labs/18/18_2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Lab
+{
+    class Toy
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Toy(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First();
+            Second();
+        }
+        static void First()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Задание 1:");
+            try
+            {
+                int count = 3;
+                Queue<int> queue1 = new Queue<int>();
+                Queue<int> queue2 = new Queue<int>();
+                Queue<int> queueMain = new Queue<int>();
+                for (int i = 1; i < count * 2; i += 2)
+                {
+                    queue1.Enqueue(i);
+                    queue2.Enqueue(i + 1);
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    queueMain.Enqueue(queue1.Dequeue());
+                    queueMain.Enqueue(queue2.Dequeue());
+                }
+
+                foreach (int item in queueMain)
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка!");
+            }
+            Console.ReadLine();
+        }
+
+        static void Second()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Задание 2:");
+            try
+            {
+                Dictionary<string, string> toys = new Dictionary<string, string>();
+                toys.Add("Раскраска", "Тысячелетний сокол");
+                toys.Add("Пазл", "Татуин");
+                toys.Add("Конструктор", "Лего: Star Wars");
+                toys = toys.OrderBy(pair => pair.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
+                foreach (var item in toys)
+                {
+                    Console.WriteLine(item.Key + " - " + item.Value);
+                }
+
+                Regex regex = new Regex(@"[А-Я]\w");
+                Console.WriteLine("Добавим игрушку в словарь:");
+                Console.Write("Тип: "); string type = Console.ReadLine();
+                Console.Write("Название: "); string name = Console.ReadLine();
+
+                while (!regex.IsMatch(name))
+                {
+                    Console.WriteLine("Название с большой буквы!");
+                    Console.Write("Тип: "); type = Console.ReadLine();
+                    Console.Write("Название: "); name = Console.ReadLine();
+                }
+
+                bool correctness = false;
+                while (!correctness)
+                {
+                    try
+                    {
+                        toys.Add(type, name);
+                        correctness = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Скорее всего такой тип уже есть!");
+                        correctness = false;
+                    }
+                }
+
+                foreach (var item in toys.ToArray())
+                {
+                    if (item.Key[0] == 'К' || item.Key[0] == 'к')
+                    {
+                        toys.Remove(item.Key);
+                    }
+                }
+                foreach (var item in toys)
+                {
+                    Console.WriteLine(item.Key + " - " + item.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetBaseException());
+            }
+            Console.ReadLine();
+        }
+    }
+}
