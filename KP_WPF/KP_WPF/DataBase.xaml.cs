b22e50382918b412925ae3c8320cf646fa1a4c2d
Alexander Grin <elirhard@mail.ru>
2019-06-07 21:09:04 +0300

    Added 30-34th labs

--- KP_WPF/KP_WPF/DataBase.xaml.cs
+++ KP_WPF/KP_WPF/DataBase.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace KP_WPF
 {
@@ -31,7 +22,29 @@ namespace KP_WPF
         private DateTime? _dateStart = null;
         private DateTime? _dateEnd = null;
         private int? _id = 0;
+        private int? _discount = 0;
         Regex regex = new Regex(@"(\+375)(44|25|33|29|)[0-9]{7}$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        DataTable dt = new DataTable();
+        public int? Discount
+        {
+            get
+            {
+                return _discount;
+            }
+            set
+            {
+                if (value >= 0 && value <= 30)
+                {
+                    _discount = value;
+                    img_tariffError.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    img_tariffError.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
         public int? ID
         {
             get
@@ -191,7 +204,7 @@ namespace KP_WPF
                 SqlCommand sqlCommand = new SqlCommand(query, connection);
                 sqlCommand.ExecuteNonQuery();
                 SqlDataAdapter sqlData = new SqlDataAdapter(sqlCommand);
-                DataTable dt = new DataTable("Bills");
+                dt = new DataTable("Bills");
                 sqlData.Fill(dt);
                 main_dataGrid.ItemsSource = dt.DefaultView;
                 sqlData.Update(dt);
@@ -218,13 +231,21 @@ namespace KP_WPF
                 {
                     Tariff = -1;
                 }
+                if (tb_discount.Text != "")
+                {
+                    Discount = int.Parse(tb_discount.Text);
+                }
+                else
+                {
+                    Discount = -1;
+                }
                 DateStart = dt_dateStart.Value;
                 DateEnd = dt_dateEnd.Value;
                 CheckInsertCorrectness();
                 if (Correctness)
                 {
                     connection.Open();
-                    string query = $"INSERT INTO [Bills] (surname, phoneNumber, date, tariff, dateStart, dateEnd) VALUES (N'{_surname}', '{_phoneNumber}', '{_date}', '{_tariff}', '{_dateStart}', '{_dateEnd}') ";
+                    string query = $"INSERT INTO [Bills] (surname, phoneNumber, date, tariff, discount, dateStart, dateEnd) VALUES (N'{_surname}', '{_phoneNumber}', '{_date}', '{_tariff}', '{_discount}', '{_dateStart}', '{_dateEnd}') ";
                     SqlCommand sqlCommand = new SqlCommand(query, connection);
                     sqlCommand.ExecuteNonQuery();
                     connection.Close();
@@ -266,6 +287,106 @@ namespace KP_WPF
             }
         }
 
+        private void GroupBY(string table)
+        {
+            try
+            {
+                connection.Open();
+                string query = $"SELECT {table}, COUNT({table}) AS [{table}Count] FROM [Bills] GROUP BY {table}";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+                dt = new DataTable("Bills");
+                sqlData.Fill(dt);
+                main_dataGrid.ItemsSource = dt.DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + ex.Source);
+            }
+        }
+
+        private void FirstEx()
+        {
+            try
+            {
+                connection.Open();
+                string query = $"Select * FROM [Bills] Order BY surname, date";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+                dt = new DataTable("Bills");
+                sqlData.Fill(dt);
+                main_dataGrid.ItemsSource = dt.DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + ex.Source);
+            }
+        }
+
+        private void SecondEx()
+        {
+            try
+            {
+                connection.Open();
+                string query = $"SELECT phoneNumber, COUNT(*) AS [Count] FROM [Bills] GROUP BY phoneNumber HAVING COUNT(*) > 1";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+                dt = new DataTable("Bills");
+                sqlData.Fill(dt);
+                main_dataGrid.ItemsSource = dt.DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + ex.Source);
+            }
+        }
+
+        private void ThirdEx()
+        {
+            try
+            {
+                connection.Open();
+                string query = $"SELECT DATEDIFF(MINUTE, dateStart, dateEnd) AS [callLength] FROM [Bills] Order by [callLength] DESC";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+                dt = new DataTable("Bills");
+                sqlData.Fill(dt);
+                main_dataGrid.ItemsSource = dt.DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + ex.Source);
+            }
+        }
+
+        private void FourthEx()
+        {
+            try
+            {
+                connection.Open();
+                string query = $"Select ROUND(MAX(tariff * CAST(discount AS float) * DATEDIFF(SECOND, dateStart, dateEnd) / 6000),2) AS [Max price] FROM [Bills]";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+                dt = new DataTable("Bills");
+                sqlData.Fill(dt);
+                main_dataGrid.ItemsSource = dt.DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + ex.Source);
+            }
+        }
+
         private void Tb_tariff_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (!float.TryParse(tb_tariff.Text, out float res))
@@ -298,6 +419,110 @@ namespace KP_WPF
             {
                 Correctness = false;
             }
+        }
+
+        private void Cb_example_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (cb_example.SelectedIndex)
+            {
+                case 0:
+                    {
+                        FirstEx();
+                        break;
+                    }
+                case 1:
+                    {
+                        SecondEx();
+                        break;
+                    }
+                case 2:
+                    {
+                        ThirdEx();
+                        break;
+                    }
+                case 3:
+                    {
+                        FourthEx();
+                        break;
+                    }
+            }
+        }
+
+        private void Cb_group_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (cb_group.SelectedIndex)
+            {
+                case 0:
+                    {
+                        GroupBY("surname");
+                        break;
+                    }
+                case 1:
+                    {
+                        GroupBY("phoneNumber");
+                        break;
+                    }
+                case 2:
+                    {
+                        GroupBY("date");
+                        break;
+                    }
+                case 3:
+                    {
+                        GroupBY("tariff");
+                        break;
+                    }
+                case 4:
+                    {
+                        GroupBY("discount");
+                        break;
+                    }
+                case 5:
+                    {
+                        GroupBY("dateStart");
+                        break;
+                    }
+                case 6:
+                    {
+                        GroupBY("dateEnd");
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        private void Button_refresh_Click(object sender, RoutedEventArgs e)
+        {
+            ShowData();
+        }
+
+        private void Tb_discount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!int.TryParse(tb_discount.Text, out int res))
+            {
+                tb_discount.Text = null;
+            }
+        }
+
+        private void Button_report_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter swExtLogFile = new StreamWriter("D:/report.txt", true);
+            int i;
+            swExtLogFile.Write(Environment.NewLine);
+            foreach (DataRow row in dt.Rows)
+            {
+                object[] array = row.ItemArray;
+                for (i = 0; i < array.Length - 1; i++)
+                {
+                    swExtLogFile.Write(array[i].ToString() + " | ");
+                }
+                swExtLogFile.WriteLine(array[i].ToString());
+            }
+            swExtLogFile.Write("*****END OF DATA****" + DateTime.Now.ToString());
+            swExtLogFile.Flush();
+            swExtLogFile.Close();
+            MessageBox.Show("Отчёт создан");
         }
     }
 }
